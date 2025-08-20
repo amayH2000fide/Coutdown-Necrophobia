ce5db41941692f39bb76ef417834733dc22a9dc1
SCA23 <scamachoarias@gmail.com>
2025-08-20 00:40:55 -0600

    Rifle bug arreglado
    Ya hace daño

--- Assets/Scripts/Gun/rifle.cs
+++ Assets/Scripts/Gun/rifle.cs
@@ -6,12 +6,22 @@ public class rifle : Gun
 {
     void Start()
     {
+
         DamageMultiplier = 0.5f;
         infiniteAmmo = false;
         fireCooldown = 0.1f;
         reloadTime = 2f;
         raycastRange = 100f;
         maxAmmo = 60;
-        currentAmmo = 60;
+        currentAmmo = maxAmmo;
+    }
+
+    void Update()
+    {
+        TickCooldown();
+        if (Input.GetButton("Fire1") && CanShoot)
+            Shoot();
+        if (Input.GetKeyDown(KeyCode.R))
+            Reload();
     }
 }
